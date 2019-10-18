2fa1b7e08edf62a2ce38dd2166b72f0aa243a5ee
javalin-CT <jcw.cybertek@gmail.com>
2019-10-18 17:30:46 -0400

    Webdriver Practice

---
+++ workspace/Selenium/src/test/java/com/cybertek/tests/Day_1_Basic_Navigation/OpenBrowserDemo.java
@@ -0,0 +1,28 @@
+package com.cybertek.tests.Day_1_Basic_Navigation;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowserDemo {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        // open a website
+        driver.get("https://cybertekschool.com/");
+
+        Thread.sleep(1000);
+
+        driver.navigate().to("https://amazon.com/");
+        System.out.println(driver.getTitle());
+        driver.navigate().back();
+
+        Thread.sleep(1000);
+        //
+        driver.navigate().forward();
+        System.out.println(driver.getTitle());
+
+    }
+}
