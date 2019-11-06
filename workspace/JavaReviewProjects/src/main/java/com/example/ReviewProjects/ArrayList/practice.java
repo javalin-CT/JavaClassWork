d19473c0a66e088c76174f97af6e30535af03aaf
javalin-CT <jcw.cybertek@gmail.com>
2019-11-05 19:16:04 -0500

    Practice

---
+++ workspace/JavaReviewProjects/src/main/java/com/example/ReviewProjects/ArrayList/practice.java
@@ -0,0 +1,27 @@
+package com.example.ReviewProjects.ArrayList;
+
+public class practice {
+    public static void main(String[] args) {
+
+
+    }
+    //delcare
+    //add(index, object)
+    //print
+    //clear
+    //get
+    //size
+    //sort
+    //set
+    //contains
+    //equals
+    //remove
+    //indexOf
+    //lastIndexOf
+    //removeAll - needs extra, cannot just .removeAll
+    //addAll
+    //asList
+    //isEmpty
+    //ArrayList to array
+    //Array to ArrayList
+}
